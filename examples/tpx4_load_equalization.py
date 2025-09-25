b17b9c9d74be3686213640d83ab9d945bc7b12ef
Mauricio Donatti <mauricio.donatti@lnls.br>
2025-09-25 14:23:57 -0300

    tpx4_load_equalization.py: add support to mask additional pixels
    improve prints and statistics

--- examples/tpx4_load_equalization.py
+++ examples/tpx4_load_equalization.py
@@ -19,6 +19,7 @@
 import sys
 import os
 import numpy as np
+from argparse import ArgumentTypeError
 
 sys.path.insert(0, os.path.join(os.getcwd(),'..','common'))
 sys.path.insert(0, os.path.join(os.getcwd(),'..'))
@@ -29,10 +30,27 @@ from spidr4 import rpc, tpx4tools
 #Import custom repository modules
 import helpers
 
+ARRAY_SIZE_X = 448
+ARRAY_SIZE_Y = 512
+
+def parse_tuples_pairs(s):
+    try:
+        parts = s.replace('(','').replace(')','').split(',')
+        if len(parts) != 2:
+            raise ValueError("Pixels must have exactly two elements.")
+        x = int(parts[0].strip())
+        if x >= ARRAY_SIZE_X: raise ValueError(f"X={x} exceeds array size {ARRAY_SIZE_X}")
+        y = int(parts[1].strip())
+        if y >= ARRAY_SIZE_Y: raise ValueError(f"Y={y} exceeds array size {ARRAY_SIZE_Y}")
+        return (x,y)
+    except ValueError as e:
+        raise ArgumentTypeError(f"Invalid format: {s}. Error: {e}")
+
 ns = helpers.cl_parse(with_chip_idx=True, args={
     '--equalization-path':dict(type=str,default='equalization',help='path to equalization files'),
     '--dac-codes-file':dict(type=str,default='eq_codes_fb.dat',help='path to dac codes file'),
     '--mask-file':dict(type=str,default='eq_mask_fb.dat',help='path to mask bit file'),
+    '--to-mask':dict(type=parse_tuples_pairs,nargs='+',help='mask additional pixels. Send pixels as tuples: (X1,Y1) (X2,Y2)'),
 })
 
 # # Main loop, create network connection
@@ -53,7 +71,7 @@ with helpers.cl_connect() as channel:
         print(f'Loading mask bits from {mask_file}')
         print('------------------------------------------------------------------------------------------------------------')
 
-        pixel_cfg_mtx = np.zeros((512, 448), dtype=np.uint8)
+        pixel_cfg_mtx = np.zeros((ARRAY_SIZE_Y, ARRAY_SIZE_X), dtype=np.uint8)
 
         #loads equalization and mask bits from file
         mask=np.loadtxt(os.path.join(ns.equalization_path,mask_file), dtype=np.bool)
@@ -61,13 +79,17 @@ with helpers.cl_connect() as channel:
 
         #configure pixels and calculate number of masked ones
         num_mask_pixels=0
-        for X in range(0,448,1):
-            for Y in range(0,512,1):
+
+        for X in range(0,ARRAY_SIZE_X,1):
+            for Y in range(0,ARRAY_SIZE_Y,1):
+                if (X,Y) in ns.to_mask:
+                    mask[X][Y] = True
+                    print(f'Masking extra pixel X={X} Y={Y}')
                 pixel_cfg_mtx[Y][X] = tpx4tools.PixelConfig(dac=equal[X][Y], power_enable=not(mask[X][Y]), tp_enable=False, mask=mask[X][Y]).word
                 #print(f'Pixel X:{X:03d} Y:{Y:03d} Equal: 0x{equal[X][Y]:02X} or {equal[X][Y]:02d} Mask: {mask[X][Y]}. Pixel cfg: 0x{pixel_cfg_mtx[Y][X]:02X} or {pixel_cfg_mtx[Y][X]:02d}')
                 if mask[X][Y]:
                     num_mask_pixels+=1
-        print(f'Num masked pixels: {num_mask_pixels}')
+        print(f'Num masked pixels: {num_mask_pixels}/{ARRAY_SIZE_X*ARRAY_SIZE_Y} = {100*num_mask_pixels/(ARRAY_SIZE_X*ARRAY_SIZE_Y):.3f}%')
 
         #Serialize pixel config data
         config_blob = tpx4tools.logic2chip_cfg_matrix(pixel_cfg_mtx)
