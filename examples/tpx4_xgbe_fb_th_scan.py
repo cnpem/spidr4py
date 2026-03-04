17556f6cc319676bda00c8210f0e4694ed6d0b29
Mauricio Donatti <mauricio.donatti@lnls.br>
2026-03-04 13:25:08 -0300

    tpx4_xgbe_fb_th_scan.py: generate a warning if a fine th scan is selected
    the user is prompted to continue

--- examples/tpx4_xgbe_fb_th_scan.py
+++ examples/tpx4_xgbe_fb_th_scan.py
@@ -99,6 +99,16 @@ ns = helpers.cl_parse(with_chip_idx=True, args={
 
 })
 
+TH_STEP_MAX = 80
+th_step = (ns.th_high_e - ns.th_low_e)/(ns.n_points-1)
+if th_step < TH_STEP_MAX:
+    print(f'WARNING: Threshold step is {th_step:.2f} electrons. Please consider to proceed with a step larger than {TH_STEP_MAX} electrons.')
+    res = input('Do you wish to continue? (y or n)\n\r')
+    if res in ['y','Y','Yes','yes']:
+        pass
+    else:
+        sys.exit(1)
+
 # # Main loop, create network connection
 with helpers.cl_connect() as channel:
 
