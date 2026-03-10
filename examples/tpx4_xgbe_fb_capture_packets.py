1795106c3c05bafd264964e9dbef0ee4f838fe77
Gustavo Siqueira <gustavo.siqueira@lnls.br>
2026-03-10 16:26:21 -0300

    tpx_xgbe_fb_capture_packets.py: Add maximum scale value in live viewer
    This change was made to allow user input of maximum scale bar value in
    live viewer as a parameter (--scale).

--- examples/tpx4_xgbe_fb_capture_packets.py
+++ examples/tpx4_xgbe_fb_capture_packets.py
@@ -54,7 +54,7 @@ def dir_path(path):
 #Define function to plot images
 def live_plot(line,img):
     line.set_data(img)
-    line.set_clim(vmin=0, vmax=np.max(img))
+    line.set_clim(vmin=0, vmax=ns.scale)
     plt.pause(0.2)
 
 
@@ -94,6 +94,7 @@ ns = helpers.cl_parse(with_chip_idx=True, args={
     '--debug':dict(type=int,choices=range(4),default=1,help='Print debug level. 0: no print, 1: standard, 2: verbose, 3: all messages'),
     "--exposure-time-us": dict(type=int,default=10,help='Exposure time (shutter time) in microseconds'),
     '--th_e':dict(type=int,default=0,help='Threshold in e-'),
+    '--scale':dict(type=int,default=1,help='Adjust maximum scale value in the live viewer plots'),
     '--auto-shutter':dict(action=BooleanOptionalAction,default=False,help='Retrigger shutter when readout finishes'),
     '--live-viewer':dict(action=BooleanOptionalAction,default=False,help='Open a simple live viewer to see current image. This can affects readout performance'),
 })
